de6600ccf530d0110ac56eae12b09b296602d0c6
nivash77 <151871666+nivash77@users.noreply.github.com>
2024-09-14 10:38:10 +0530

    Update testingjs.js

--- testing-fullstack/testingjs.js
+++ testing-fullstack/testingjs.js
@@ -28,3 +28,25 @@ function myfunction(e) {
     }
 
 }
+function myfunction(e) {
+    e.preventDefault();  // Prevent the form from reloading
+    var name = document.getElementById("name").value;
+    let age=document.getElementById("age").value;
+    let mail = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+    if(name!="" && mail!="" &&pass!=""&& age!=0 && age!=null){
+        
+        let userlist = {
+            username: name,
+            mailid: mail,
+            password: pass,
+            userAge:(age>=18)? "Major":"Minor"
+        };
+        console.log(userlist); 
+
+    }
+    else{
+        alert("Not give empty value");
+    }
+
+}
