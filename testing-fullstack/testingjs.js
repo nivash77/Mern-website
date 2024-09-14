b4c3b47e17d61a002fa344997236d0f2aeb43064
nivash77 <151871666+nivash77@users.noreply.github.com>
2024-09-14 10:41:42 +0530

    Update testingjs.js

--- testing-fullstack/testingjs.js
+++ testing-fullstack/testingjs.js
@@ -28,25 +28,30 @@ function myfunction(e) {
     }
 
 }
-function myfunction(e) {
-    e.preventDefault();  // Prevent the form from reloading
-    var name = document.getElementById("name").value;
-    let age=document.getElementById("age").value;
-    let mail = document.getElementById("email").value;
-    let pass = document.getElementById("password").value;
-    if(name!="" && mail!="" &&pass!=""&& age!=0 && age!=null){
-        
-        let userlist = {
-            username: name,
-            mailid: mail,
-            password: pass,
-            userAge:(age>=18)? "Major":"Minor"
-        };
-        console.log(userlist); 
-
-    }
-    else{
-        alert("Not give empty value");
-    }
-
+function success(){
+    console.log("Success")
 }
+function fail(){
+    console.log("Fail")
+}
+function error(){
+    console.log("error")
+}
+function exec(data,callback){
+    if(data){
+        console.log(data)
+    }
+    setTimeout(callback,3000)
+}
+function trigger(data){
+if(data>0){
+    exec(data,success)
+}
+else if(data<=0){
+    exec(data,fail)
+}
+else{
+    exec(data,error)
+}
+}
+trigger(3)
