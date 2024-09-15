69bfbf6dc52c4d5bed785a0caf37f16b4d9c0b90
nivash77 <151871666+nivash77@users.noreply.github.com>
2024-09-15 18:28:55 +0530

    Add files via upload

--- testing-fullstack/api.js
+++ testing-fullstack/api.js
@@ -3,11 +3,18 @@ async function API() {
     try{
     const res= await fetch('https://66e527625cc7f9b6273c701a.mockapi.io/endpoint/register')
     const data=await res.json()
-    console.table(data)
+    return data;
     }
     catch(error){
         console.log(error);
     }
     
 }
-API()
+async function APIDATA() {
+    const apidata=await API()
+    const isact=apidata.filter(data=>data.isActive)
+    console.log(isact)
+    const str=apidata.map((data)=>(console.log(data)))
+    
+}
+APIDATA()
